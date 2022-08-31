fb132e657e7e1924af2c7c04fd2f559b01b36c07
m1m1m <m1m1mkas@mail.ru>
2022-08-31 15:14:29 +0300

    seminar_2

---
+++ Seminar2/Task_1.cs
@@ -0,0 +1,6 @@
+﻿System.Console.WriteLine();
+int number = new Random().Next(100, 999);
+
+System.Console.WriteLine($"Второй цифрой числа {number} является {number / 10 % 10} ");
+
+System.Console.WriteLine(); 
---
+++ Task_2/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine();
+
+int Number = new Random().Next(1, 65536);
+
+Console.WriteLine("Дано число: {Number}");
+
+if (Number / 100 > 0) Console.WriteLine($"Третьей цифрой числа {Number} является цифра {Number / 100 % 10}");
+else Console.WriteLine("У числа {Number} нет третьей цифры");
+
+Console.WriteLine();
